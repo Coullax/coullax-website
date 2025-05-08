ceb20d79ec5109e6933eb92d4c1f559ff41d7061
bhashanasirimanna <bhashanasirimanna@gmail.com>
2025-05-08 13:21:33 +0530

    add new icons and enhance layout structure for Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,13 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import { CubeTransparent, Lightning, ShieldCheck } from "@phosphor-icons/react";
+import {
+  ArrowLineUpRight,
+  Brain,
+  CubeTransparent,
+  Lightning,
+  ShieldCheck,
+} from "@phosphor-icons/react";
 import { ArrowRight } from "lucide-react";
 
 export default function Home() {
@@ -14,7 +20,7 @@ export default function Home() {
   ];
 
   return (
-    <div className=" w-full p-[50px] bg-[#fffef1]">
+    <div className=" w-full pt-[50px] bg-[#fffef1]">
       <div className=" w-full">
         <div className="w-[90%] max-w-[1200px] flex items-center justify-between mx-auto">
           <div>
@@ -31,8 +37,10 @@ export default function Home() {
 
           <div className="flex justify-center items-center">
             <div className="flex justify-center items-center gap-[35px] font-bold leading-[23px] font-stretch-normal font-plus-jakarta-sans text-[20px]">
-              {navigationList.map((navigation,index) => (
-                <Link key={index} href={navigation.href}>{navigation.name}</Link>
+              {navigationList.map((navigation, index) => (
+                <Link key={index} href={navigation.href}>
+                  {navigation.name}
+                </Link>
               ))}
             </div>
           </div>
@@ -43,7 +51,11 @@ export default function Home() {
         <div className=" w-[90%] max-w-[1200px] mx-auto my-[140px] ">
           <div className="bg-[#1e201f] rounded-[25px] px-[80px] py-[57px]">
             <div className=" flex flex-row justify-start items-center gap-[20px] ">
-              <Lightning size={32} weight="bold" className="text-white ml-[16px]" />
+              <Lightning
+                size={32}
+                weight="bold"
+                className="text-white ml-[16px]"
+              />
               <ShieldCheck size={32} weight="bold" className="text-white" />
               <CubeTransparent size={32} weight="bold" className="text-white" />
             </div>
@@ -84,66 +96,89 @@ export default function Home() {
                   This is where we ace the game
                 </span>
               </div>
-              <div className="mt-[45px] h-[69px] flex-shrink-0  text-[clamp(16px,5vw,20px)] font-medium not-italic leading-[1.16] tracking-[normal] text-left text-[#575757]">
-                Working with us is easy We follow a clear no fuss process that keeps things moving fast and smooth, from first chat to final delivery.
+
               <div className="mt-[45px] text-[#575757] font-medium text-[20px] leading-[23px]">
-                Working with us is easy We follow a clear no fuss process that<br/>
-                keeps things moving fast and smooth, from first chat to final<br/>
+                Working with us is easy We follow a clear no fuss process that
+                <br />
+                keeps things moving fast and smooth, from first chat to final
+                <br />
                 delivery.
               </div>
-            </div>
 
-            <div className="w-full flex flex-col md:flex-row justify-start items-center gap-[40px] p-0 my-[40px]">
-              <div className="w-full max-w-[580px] flex-grow-0 object-contain">
-                <Image
-                  src="/Homepage/ArtificalInteligence.png"
-                  alt="image1"
-                  width={580}
-                  height={357}
-                  className="w-full h-full object-contain"
-                />
-              </div>
-              <div className="w-full max-w-[580px] flex-grow-0 object-contain">
-                <Image
-                  src="/Homepage/BlockChaain.png"
-                  alt="image1"
-                  width={580}
-                  height={357}
-                  className="w-full h-full object-contain"
-                />
-              </div>
-            </div>
-
-            <div className="w-full  flex flex-col md:flex-row justify-between items-center gap-[25px] p-0 ">
-              <div className="w-full flex-grow-0 flex flex-col justify-start items-start gap-[20px] px-[25px] py-[24px] rounded-[25px] bg-[#def7ff]">
-                <span className=" self-stretch flex-grow-0  text-[20px] font-bold not-italic leading-[1.16] tracking-[-1px] text-left text-[#2a3b40]">
-                  Web & Software Development
-                </span>
-                <div className="self-stretch w-full max-w-[188px] flex-grow-0 flex flex-col justify-start items-stretch gap-[10.8px] p-0">
-                  Automation Solutions Websites & Software Mobile Apps
+              <div className="w-full flex flex-col md:flex-row justify-start items-center gap-[40px] my-[40px]">
+                <div className=" bg-[#549f99] flex flex-col justify-between rounded-[25px] min-h-[385px] md:w-[50%] ">
+                  <div className=" w-full flex justify-between  px-[45px] py-[30px]">
+                    <Brain size={70} color="#ffffff" weight="regular" />
+                    <div>
+                      <ArrowLineUpRight size={35} color="#ffffff" />
+                      <div className=" mt-[15px] flex flex-col justify-start items-start">
+                        <div className=" h-[42px] aspect-square bg-[#d1ecb1]"></div>
+                        <div className=" h-[42px] aspect-square bg-[#e3ffe6]"></div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className=" bg-[#d1ecb1] px-[34px] h-[121px] rounded-b-[25px] flex justify-start items-center">
+                    <span className=" text-[36px] -tracking-[1.8px] leading-[42px] font-bold text-[#0b251a]">
+                      Artificial
+                      <br />
+                      INTELLIGENCE
+                    </span>
+                  </div>
+                </div>
+                <div className=" bg-[#605ec2] flex flex-col justify-between rounded-[25px] min-h-[385px] md:w-[50%] ">
+                  <div className=" w-full flex justify-between  px-[45px] py-[30px]">
+                    <div>
+                      <CubeTransparent
+                        size={76}
+                        color="#ffffff"
+                        weight="regular"
+                      />
+                      <div className=" mt-[15px] flex flex-col justify-start items-start">
+                        <div className=" h-[21px] aspect-square bg-[#343377]"></div>
+                        <div className=" h-[21px] aspect-square bg-[#e1e2ff]"></div>
+                      </div>
+                    </div>
+                    <ArrowLineUpRight size={35} color="#ffffff" />
+                  </div>
+                  <div className=" bg-[#b9b8ef] px-[34px] h-[121px] rounded-b-[25px] flex justify-start items-center">
+                    <span className=" text-[36px] -tracking-[1.8px] leading-[42px] font-bold text-[#343377]">
+                      Blockchain
+                      <br />
+                      TECHNOLOGY
+                    </span>
+                  </div>
                 </div>
               </div>
-              <div className="w-full flex-grow-0 flex flex-col justify-start items-start gap-[20px] px-[25px] py-[24px] rounded-[25px] bg-[#dbdcfd]">
-                <span className=" self-stretch flex-grow-0  text-[20px] font-bold not-italic leading-[1.16] tracking-[-1px] text-left text-[#2a3b40]">
-                  Web & Software Development
-                </span>
-                <div className="self-stretch w-full max-w-[188px] flex-grow-0 flex flex-col justify-start items-stretch gap-[10.8px] p-0">
-                  Automation Solutions Websites & Software Mobile Apps
+
+              <div className="w-full  flex flex-col md:flex-row justify-between items-center gap-[25px] p-0 ">
+                <div className="w-full flex-grow-0 flex flex-col justify-start items-start gap-[20px] px-[25px] py-[24px] rounded-[25px] bg-[#def7ff]">
+                  <span className=" self-stretch flex-grow-0  text-[20px] font-bold not-italic leading-[1.16] tracking-[-1px] text-left text-[#2a3b40]">
+                    Web & Software Development
+                  </span>
+                  <div className="self-stretch w-full max-w-[188px] flex-grow-0 flex flex-col justify-start items-stretch gap-[10.8px] p-0">
+                    Automation Solutions Websites & Software Mobile Apps
+                  </div>
                 </div>
-              </div>
-              <div className="w-full  flex-grow-0 flex flex-col justify-start items-start gap-[20px] px-[25px] py-[24px] rounded-[25px] bg-[#d6f2e8]">
-                <span className=" self-stretch flex-grow-0  text-[20px] font-bold not-italic leading-[1.16] tracking-[-1px] text-left text-[#2a3b40]">
-                  Web & Software Development
-                </span>
-                <div className="self-stretch w-full max-w-[188px]  flex-grow-0 flex flex-col justify-start items-stretch gap-[10.8px] p-0">
-                  Automation Solutions Websites & Software Mobile Apps
+                <div className="w-full flex-grow-0 flex flex-col justify-start items-start gap-[20px] px-[25px] py-[24px] rounded-[25px] bg-[#dbdcfd]">
+                  <span className=" self-stretch flex-grow-0  text-[20px] font-bold not-italic leading-[1.16] tracking-[-1px] text-left text-[#2a3b40]">
+                    Web & Software Development
+                  </span>
+                  <div className="self-stretch w-full max-w-[188px] flex-grow-0 flex flex-col justify-start items-stretch gap-[10.8px] p-0">
+                    Automation Solutions Websites & Software Mobile Apps
+                  </div>
+                </div>
+                <div className="w-full  flex-grow-0 flex flex-col justify-start items-start gap-[20px] px-[25px] py-[24px] rounded-[25px] bg-[#d6f2e8]">
+                  <span className=" self-stretch flex-grow-0  text-[20px] font-bold not-italic leading-[1.16] tracking-[-1px] text-left text-[#2a3b40]">
+                    Web & Software Development
+                  </span>
+                  <div className="self-stretch w-full max-w-[188px]  flex-grow-0 flex flex-col justify-start items-stretch gap-[10.8px] p-0">
+                    Automation Solutions Websites & Software Mobile Apps
+                  </div>
                 </div>
               </div>
             </div>
-
           </div>
         </div>
-      </div>
 
 
 
