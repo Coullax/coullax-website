b1b64e11ec24d1b5343666b351955eb8d4e4b2db
Madhawa Awishka <madhawaawishka@gmail.com>
2025-05-14 17:16:54 +0530

    Improve CareersPage layout with responsive design adjustments and accessibility enhancements

--- src/app/careers/page.tsx
+++ src/app/careers/page.tsx
@@ -21,9 +21,19 @@ interface CarrouseImageItem {
     image: StaticImageData;
     width: number;
     height: number;
-}   
+}
 
 export default function CareersPage() {
+    const skills = [
+        { name: 'Software Engineering', isActive: false },
+        { name: 'Design', isActive: false },
+        { name: 'Quality Assurance', isActive: false },
+        { name: 'Project Management', isActive: false },
+        { name: 'User Experience Research', isActive: true },
+        { name: 'User Experience Research', isActive: true },
+        { name: 'User Experience Research', isActive: true },
+
+    ];
   const processList: ProcessList[] = [
     {
       title: "Your Resume",
@@ -68,62 +78,239 @@ export default function CareersPage() {
         <div className=" w-full pt-[50px] bg-[#fff]">
             <div className=" w-full">
                 <div className="w-[90%] max-w-[1200px] mx-auto">
-                    <HeaderNavBar />
+                    <HeaderNavBar/>
                 </div>
             </div>
 
-      <div className="w-full relative overflow-hidden">
-        <div className=" w-[90%] max-w-[1200px] mx-auto my-[70px] md:my-[140px] ">
-          <div className="flex flex-row bg-[#6abb79] items-start gap-1.5 rounded-[100px] w-fit py-[5px] pl-[5px] pr-[14px]">
-            <ArrowCircleRight size={24} />
-            <span className="text-[20px] leading-[23px] -tracking-[1px] text-left text-[#1e201f] font-semibold">
+            <div className="w-full relative overflow-hidden">
+                <div className=" w-[90%] max-w-[1200px] mx-auto mt-[70px] md:mt-[140px] ">
+                    <div
+                        className="flex flex-row bg-[#6abb79] items-start gap-1.5 rounded-[100px] w-fit py-[5px] pl-[5px] pr-[14px]">
+                        <ArrowCircleRight size={24}/>
+                        <span
+                            className="text-[20px] leading-[23px] -tracking-[1px] text-left text-[#1e201f] font-semibold">
               We are Hiring
             </span>
-          </div>
-          <h1 className=" text-black my-[24px] lg:my-[32px] leading-[47px] -tracking-[2.2px] text-[36px] lg:text-[45px] font-bold text-left">
-            Careers
-          </h1>
-          <div className="text-left text-[20px] lg:text-[24px] text-[#575757] font-medium leading-[28px] max-w-[760px]">
-            Our mission is to enhance efficiency, security, and transparency
-            across industries. Making the world a smarter, more connected, and a
-            safer place.
-            <br />
-            <br />
-            To achieve this, we believe in the power of Artificial
-            intelligence&apos;s efficiency and Blockchain technology&apos;s
-            security and transparency
-          </div>
-          <div className=" mt-[24px] lg:mt-[32px]">
-            <Marquee pauseOnHover className="[--duration:20s] p-0">
-              {carrouseImageList.map((image: CarrouseImageItem, index: number) => (
-                <Image
-                  key={index}
-                  src={image.image}
-                  alt={`image${index}`}
-                  width={image.width}
-                  height={image.height}
-                  className=" rounded-[15px] bg-[#d9d9d9] mr-[32px]"
-                />
-              ))}
-            </Marquee>
-          </div>
-        </div>
-      </div>
+                    </div>
+                    <h1 className=" text-black my-[24px] lg:my-[32px] leading-[47px] -tracking-[2.2px] text-[36px] lg:text-[45px] font-bold text-left">
+                        Careers
+                    </h1>
+                    <div
+                        className="text-left text-[20px] lg:text-[24px] text-[#575757] font-medium leading-[28px] max-w-[760px]">
+                        Our mission is to enhance efficiency, security, and transparency
+                        across industries. Making the world a smarter, more connected, and a
+                        safer place.
+                        <br/>
+                        <br/>
+                        To achieve this, we believe in the power of Artificial
+                        intelligence&apos;s efficiency and Blockchain technology&apos;s
+                        security and transparency
+                    </div>
+                    <div className=" mt-[24px] lg:mt-[32px]">
+                        <Marquee pauseOnHover className="[--duration:20s] p-0">
+                            {carrouseImageList.map((image: CarrouseImageItem, index: number) => (
+                                <Image
+                                    key={index}
+                                    src={image.image}
+                                    alt={`image${index}`}
+                                    width={image.width}
+                                    height={image.height}
+                                    className=" rounded-[15px] bg-[#d9d9d9] mr-[32px]"
+                                />
+                            ))}
+                        </Marquee>
+                    </div>
+                </div>
+            </div>
 
-            <div className="w-full relative bg-gradient-to-t from-[#d1ecb1] to-[#fff] overflow-hidden mb-[40px] md:mb-[140px] pb-[85px]">
+
+            <div className="w-full relative overflow-hidden">
+                <div className=" w-[90%] max-w-[1200px] mx-auto my-[70px] md:my-[140px] ">
+
+                    <div className='flex flex-col '>
+                        <div className='flex overflow-y-auto gap-[20px] hide-scrollbar'>
+                            {skills.map((skill, index) => (
+                                <div
+                                    key={index}
+                                    className={`flex shrink-0 justify-around rounded-[100px] text-[20px] font-[600] p-[15px] bg-[#f1f6f1] w-fit1`}
+                                >
+                                    {skill.name}
+                                </div>
+                            ))}
+
+
+                        </div>
+
+
+                        <div
+                            className='grid lg:grid-cols-3 mt-[90px] gap-[30px] md:grid-cols-2 grid-cols-1 justify-center items-center'>
+
+                            <div className='flex flex-col min-w-[380px] bg-[#f1f6f1] rounded-3xl p-6 '>
+                                <span className='text-2xl font-[600] mb-[16px]'>Product Designer</span>
+                                <div className='text-[16px] w-[240px] text-left font-normal mb-[30px]'>
+                                    We are hiring a expert product designer for our team
+                                </div>
+                                <div className='flex flex-row justify-start gap-[10px] mb-[30px]'>
+                                    <div
+                                        className='flex flex-row justify-center items-center gap-[4.8px] px-[10px] py-[5px] rounded-[50px]  border-[2px] border-[#000]   min-w-[70px] text-[14px] font-semibold'>Full
+                                        Time
+                                    </div>
+                                    <div
+                                        className='flex flex-row justify-center items-center gap-[4.8px] px-[10px] py-[5px] rounded-[50px]  border-[2px] border-[#000]  text-[14px] min-w-[70px] font-semibold'>on
+                                        Site
+                                    </div>
+                                </div>
+                                <div className='flex justify-end'>
+                                    <div
+                                        className='flex flex-row justify-center items-center p-1 bg-[#000] max-w-[135px] rounded-[100px] cursor-pointer px-1.5'>
+                                        <span className='text-[#ffffff] text-[16px] font-[600px] mx-[15px] my-[5px]'>Apply Now</span>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className='flex flex-col min-w-[380px] bg-[#f1f6f1] rounded-3xl p-6 '>
+                                <span className='text-2xl font-[600] mb-[16px]'>Product Designer</span>
+                                <div className='text-[16px] w-[240px] text-left font-normal mb-[30px]'>
+                                    We are hiring a expert product designer for our team
+                                </div>
+                                <div className='flex flex-row justify-start gap-[10px] mb-[30px]'>
+                                    <div
+                                        className='flex flex-row justify-center items-center gap-[4.8px] px-[10px] py-[5px] rounded-[50px]  border-[2px] border-[#000]   min-w-[70px] text-[14px] font-semibold'>Full
+                                        Time
+                                    </div>
+                                    <div
+                                        className='flex flex-row justify-center items-center gap-[4.8px] px-[10px] py-[5px] rounded-[50px]  border-[2px] border-[#000]  text-[14px] min-w-[70px] font-semibold'>on
+                                        Site
+                                    </div>
+                                </div>
+                                <div className='flex justify-end'>
+                                    <div
+                                        className='flex flex-row justify-center items-center p-1 bg-[#000] max-w-[135px] rounded-[100px] cursor-pointer px-1.5'>
+                                        <span className='text-[#ffffff] text-[16px] font-[600px] mx-[15px] my-[5px]'>Apply Now</span>
+                                    </div>
+                                </div>
+                            </div>
+
+
+                            <div className='flex flex-col min-w-[380px] bg-[#f1f6f1] rounded-3xl p-6 '>
+                                <span className='text-2xl font-[600] mb-[16px]'>Product Designer</span>
+                                <div className='text-[16px] w-[240px] text-left font-normal mb-[30px]'>
+                                    We are hiring a expert product designer for our team
+                                </div>
+                                <div className='flex flex-row justify-start gap-[10px] mb-[30px]'>
+                                    <div
+                                        className='flex flex-row justify-center items-center gap-[4.8px] px-[10px] py-[5px] rounded-[50px]  border-[2px] border-[#000]   min-w-[70px] text-[14px] font-semibold'>Full
+                                        Time
+                                    </div>
+                                    <div
+                                        className='flex flex-row justify-center items-center gap-[4.8px] px-[10px] py-[5px] rounded-[50px]  border-[2px] border-[#000]  text-[14px] min-w-[70px] font-semibold'>on
+                                        Site
+                                    </div>
+                                </div>
+                                <div className='flex justify-end'>
+                                    <div
+                                        className='flex flex-row justify-center items-center p-1 bg-[#000] max-w-[135px] rounded-[100px] cursor-pointer px-1.5'>
+                                        <span className='text-[#ffffff] text-[16px] font-[600px] mx-[15px] my-[5px]'>Apply Now</span>
+                                    </div>
+                                </div>
+                            </div>
+
+
+                            <div className='flex flex-col min-w-[380px] bg-[#f1f6f1] rounded-3xl p-6 '>
+                                <span className='text-2xl font-[600] mb-[16px]'>Product Designer</span>
+                                <div className='text-[16px] w-[240px] text-left font-normal mb-[30px]'>
+                                    We are hiring a expert product designer for our team
+                                </div>
+                                <div className='flex flex-row justify-start gap-[10px] mb-[30px]'>
+                                    <div
+                                        className='flex flex-row justify-center items-center gap-[4.8px] px-[10px] py-[5px] rounded-[50px]  border-[2px] border-[#000]   min-w-[70px] text-[14px] font-semibold'>Full
+                                        Time
+                                    </div>
+                                    <div
+                                        className='flex flex-row justify-center items-center gap-[4.8px] px-[10px] py-[5px] rounded-[50px]  border-[2px] border-[#000]  text-[14px] min-w-[70px] font-semibold'>on
+                                        Site
+                                    </div>
+                                </div>
+                                <div className='flex justify-end'>
+                                    <div
+                                        className='flex flex-row justify-center items-center p-1 bg-[#000] max-w-[135px] rounded-[100px] cursor-pointer px-1.5'>
+                                        <span className='text-[#ffffff] text-[16px] font-[600px] mx-[15px] my-[5px]'>Apply Now</span>
+                                    </div>
+                                </div>
+                            </div>
+
+
+                            <div className='flex flex-col min-w-[380px] bg-[#f1f6f1] rounded-3xl p-6 '>
+                                <span className='text-2xl font-[600] mb-[16px]'>Product Designer</span>
+                                <div className='text-[16px] w-[240px] text-left font-normal mb-[30px]'>
+                                    We are hiring a expert product designer for our team
+                                </div>
+                                <div className='flex flex-row justify-start gap-[10px] mb-[30px]'>
+                                    <div
+                                        className='flex flex-row justify-center items-center gap-[4.8px] px-[10px] py-[5px] rounded-[50px]  border-[2px] border-[#000]   min-w-[70px] text-[14px] font-semibold'>Full
+                                        Time
+                                    </div>
+                                    <div
+                                        className='flex flex-row justify-center items-center gap-[4.8px] px-[10px] py-[5px] rounded-[50px]  border-[2px] border-[#000]  text-[14px] min-w-[70px] font-semibold'>on
+                                        Site
+                                    </div>
+                                </div>
+                                <div className='flex justify-end'>
+                                    <div
+                                        className='flex flex-row justify-center items-center p-1 bg-[#000] max-w-[135px] rounded-[100px] cursor-pointer px-1.5'>
+                                        <span className='text-[#ffffff] text-[16px] font-[600px] mx-[15px] my-[5px]'>Apply Now</span>
+                                    </div>
+                                </div>
+                            </div>
+
+
+                            <div className='flex flex-col min-w-[380px] bg-[#f1f6f1] rounded-3xl p-6 '>
+                                <span className='text-2xl font-[600] mb-[16px]'>Product Designer</span>
+                                <div className='text-[16px] w-[240px] text-left font-normal mb-[30px]'>
+                                    We are hiring a expert product designer for our team
+                                </div>
+                                <div className='flex flex-row justify-start gap-[10px] mb-[30px]'>
+                                    <div
+                                        className='flex flex-row justify-center items-center gap-[4.8px] px-[10px] py-[5px] rounded-[50px]  border-[2px] border-[#000]   min-w-[70px] text-[14px] font-semibold'>Full
+                                        Time
+                                    </div>
+                                    <div
+                                        className='flex flex-row justify-center items-center gap-[4.8px] px-[10px] py-[5px] rounded-[50px]  border-[2px] border-[#000]  text-[14px] min-w-[70px] font-semibold'>on
+                                        Site
+                                    </div>
+                                </div>
+                                <div className='flex justify-end'>
+                                    <div
+                                        className='flex flex-row justify-center items-center p-1 bg-[#000] max-w-[135px] rounded-[100px] cursor-pointer px-1.5'>
+                                        <span className='text-[#ffffff] text-[16px] font-[600px] mx-[15px] my-[5px]'>Apply Now</span>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+
+
+                </div>
+            </div>
+
+            <div
+                className="w-full relative bg-gradient-to-t from-[#d1ecb1] to-[#fff] overflow-hidden mb-[40px] md:mb-[140px] pb-[85px]">
                 <div className=" w-[90%] max-w-[1200px] mx-auto  ">
                     <div className="flex flex-col justify-center items-center gap-[54px] p-0">
                         <div className="w-full flex flex-col justify-between items-center">
-                            <div className=" flex flex-row justify-center items-center rounded-full py-[10px] px-[22px]">
+                            <div
+                                className=" flex flex-row justify-center items-center rounded-full py-[10px] px-[22px]">
                                 <span className="w-full font-bold leading-[23px] text-[24px] text-[#000]">
                                     Our hiring process
                                 </span>
                             </div>
-                            <span className="w-full mt-[45px] text-[20px] font-medium leading-[23px] text-center text-[#575757]">
+                            <span
+                                className="w-full mt-[45px] text-[20px] font-medium leading-[23px] text-center text-[#575757]">
                                 Working with us is easy. We follow a clear, no fuss process 
-                                <br />
+                                <br/>
                                 that keeps things moving fast and smooth, from first chat to 
-                                <br />
+                                <br/>
                                 final delivery.
                             </span>
                         </div>
@@ -165,7 +352,7 @@ export default function CareersPage() {
                 </VelocityScroll>
             </div>
 
-      <Footer />
-    </div>
-  );
+            <Footer/>
+        </div>
+    );
 }
