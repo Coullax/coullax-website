8890bc1a7ad42f82de055d967abca53777ca4dd8
Madhawa Awishka <madhawaawishka@gmail.com>
2025-06-13 15:01:56 +0530

    Adjust layout and styling in CareersPage for improved responsiveness and visual consistency

--- src/app/careers/page.tsx
+++ src/app/careers/page.tsx
@@ -287,7 +287,7 @@ export default function CareersPage() {
             </div>
 
 
-            <div className="w-[90%] max-w-[1550px] mx-auto flex justify-between mt-[200px]">
+            <div className="w-[90%] max-w-[1550px] mx-auto flex justify-between lg:mt-[200px] mt-[100px]">
               <span className="text-[26px] lg:text-[66px] md:text-[46px] uppercase font-special-gothic-expanded-one lg:-tracking-[4.8px]">The hand of<br></br> entity</span>
               <span className=" lg:text-[36px] text-[16px]  md:text-[26px] uppercase font-special-gothic-expanded-one">careers</span>
             </div>
@@ -301,7 +301,7 @@ export default function CareersPage() {
             Coullax seeks thinkers, makers, and system breakers , <br></br>individuals drawn to the strange, the powerful, and <br></br>the unseen mechanics behind what we build. Whether <br></br>you wield code like language, design like ritual, or<br></br> strategy like signal alignment, there is a place for<br></br> you within the Forge.
           </div>
 
-          <button className=" bg-[#e0ef29] min-w-[312px] py-[25px] px-[30px] text-[20px] text-center leading-[20px] -tracking-[1px] uppercase font-silkscreen text-[#030303] border-dashed border-black mt-[20px] lg:mt-[50px] my-[50px] lg:my-[100px] border-3 hover:bg-transparent hover:text-[#000000] hover:border-[#000000]  cursor-pointer transition-all duration-300 hover:scale-105">
+          <button className=" bg-[#e0ef29] min-w-[312px] lg:py-[25px] py-[15px] lg:px-[30px] px-[10px] text-[20px] text-center leading-[20px] -tracking-[1px] uppercase font-silkscreen text-[#030303] border-dashed border-black mt-[20px] lg:mt-[50px] my-[50px] lg:my-[100px] border-3 hover:bg-transparent hover:text-[#000000] hover:border-[#000000]  cursor-pointer transition-all duration-300 hover:scale-105">
             SEND US YOUR RESUME
           </button>
         </div>
@@ -394,7 +394,7 @@ export default function CareersPage() {
                 Joining Coullax is not a transaction — it is a<br></br>transition. We don&apos;t merely review resumes; we<br></br>decipher intent, resonance, and potential. Our<br></br>process is built to uncover builders who think<br></br> beyond the visible . individuals who feel the pulse<br></br> of systems not yet formed.
               </div>
 
-              <button className="bg-[#e0ef29] min-w-[312px] lg:py-[25px] py-[15px] lg:px-[30px] px-[10px] lg:text-[20px] text-[15px] text-center leading-[20px] -tracking-[1px] uppercase font-silkscreen text-[#030303]  border-dashed border-black mt-[50px] my-[100px] border-2 hover:bg-transparent hover:text-[#e0ef29] hover:border-[#e0ef29] hover:scale-105  transition-all duration-300 cursor-pointer">
+              <button className="bg-[#e0ef29] min-w-[312px]  lg:py-[25px] py-[15px] lg:px-[30px] px-[10px] lg:text-[20px] text-[15px] text-center  -tracking-[1px] uppercase font-silkscreen text-[#030303]  border-dashed border-black mt-[50px] my-[100px] border-2 hover:bg-transparent hover:text-[#e0ef29] hover:border-[#e0ef29] hover:scale-105  transition-all duration-300 cursor-pointer">
                 SEND US YOUR RESUME
               </button>
             </div>
@@ -405,7 +405,7 @@ export default function CareersPage() {
                   key={index}
                   className="border border-white flex flex-col py-[31.5px] px-[32.8px] hover:bg-[#e0ef29] group"
                 >
-                  <span className="font-special-gothic-expanded-one text-[40px] text-white group-hover:text-black -tracking-[2px]">
+                  <span className="font-special-gothic-expanded-one lg:text-[40px] text-[30px] text-white group-hover:text-black lg:-tracking-[2px] -tracking-[1px]">
                     {step.title}
                   </span>
                   <span className="font-inclusive-sans text-[24px] text-white mt-[43px] group-hover:text-black uppercase -tracking-[0.2px]">
