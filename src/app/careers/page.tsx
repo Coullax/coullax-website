ffa1a5a212d1c3f152875e222cbf5a893a6bbffd
Madhawa Awishka <madhawaawishka@gmail.com>
2025-06-13 12:43:01 +0530

    Refactor Footer component for better accessibility and improved layout

--- src/app/careers/page.tsx
+++ src/app/careers/page.tsx
@@ -2,20 +2,12 @@
 import Footer from "@/components/Footer";
 import HeaderNavBar from "@/components/Header";
 import { Marquee } from "@/components/magicui/marquee";
-// import { VelocityScroll } from "@/components/magicui/scroll-based-velocity";
 import {ArrowUpRight } from "@phosphor-icons/react";
 
 import Image, { StaticImageData } from 'next/image';
 import React from 'react';
 import Image1 from "../../../public/careersPage/image1.png"
 import Image2 from "../../../public/careersPage/image2.png";
-
-// interface ProcessList {
-//     title: string;
-//     backGround: string;
-//     textColor: string;
-//     description: string;
-// }
 
 interface CarrouseImageItem {
     image: StaticImageData;
@@ -284,29 +276,11 @@ export default function CareersPage() {
 
   ];
 
-  // const handleSkillClick = (clickedIndex: number) => {
-  //   const newSkills = skills.map((skill, index) => ({
-  //     ...skill,
-  //     isActive: index === clickedIndex ? !skill.isActive : false
-  //   }));
-    
-  //   setSkills(newSkills);
-    
-  //   const clickedSkill = newSkills[clickedIndex];
-  //   if (clickedSkill.isActive) {
-  //     // setSelectedSkill(clickedSkill.name);
-  //     const foundSkill = skillJobs.find(skill => skill.name === clickedSkill.name);
-  //     setActiveJobs(foundSkill ? foundSkill.jobs : []);
-  //   } else {
-  //     // setSelectedSkill(null);
-  //     setActiveJobs(skillJobs.flatMap(skill => skill.jobs)); // Show all jobs when no skill selected
-  //   }
-  // };
 
 
     return (
         <div className=" w-full pt-[50px] bg-[#fff]">
-            <div className=" w-full">
+            <div className="w-full">
                 <div className="w-[90%] max-w-[1550px] mx-auto">
                     <HeaderNavBar/>
                 </div>
@@ -314,27 +288,27 @@ export default function CareersPage() {
 
 
             <div className="w-[90%] max-w-[1550px] mx-auto flex justify-between mt-[200px]">
-              <span className="text-[96px] uppercase font-special-gothic-expanded-one -tracking-[4.8px]">The hand of<br></br> entity</span>
-              <span className="text-[36px] uppercase font-special-gothic-expanded-one">careers</span>
+              <span className="text-[26px] lg:text-[66px] md:text-[46px] uppercase font-special-gothic-expanded-one lg:-tracking-[4.8px]">The hand of<br></br> entity</span>
+              <span className=" lg:text-[36px] text-[16px]  md:text-[26px] uppercase font-special-gothic-expanded-one">careers</span>
             </div>
 
         <div className="w-[90%] max-w-[1550px]  mx-auto mt-[30px] text-left">
-          <div className=" text-[15px] leading-[20px] uppercase font-silkscreen text-black">
-          To build the unbuilt, we gather the rare, the restless, the aligne
+          <div className="lg:text-[15px]  text-[13px]  leading-[20px] uppercase font-silkscreen text-black">
+            To build the unbuilt, we gather the rare, the restless, the aligne
           </div>
 
-          <div className=" text-[40px] mt-[56px] uppercase font-inclusive-sans text-black">
-          Coullax seeks thinkers, makers, and system breakers , <br></br>individuals drawn to the strange, the powerful, and <br></br>the unseen mechanics behind what we build. Whether <br></br>you wield code like language, design like ritual, or<br></br> strategy like signal alignment, there is a place for<br></br> you within the Forge.
+          <div className="text-[20px] md:text-[40px] mt-[56px] uppercase font-inclusive-sans text-black">
+            Coullax seeks thinkers, makers, and system breakers , <br></br>individuals drawn to the strange, the powerful, and <br></br>the unseen mechanics behind what we build. Whether <br></br>you wield code like language, design like ritual, or<br></br> strategy like signal alignment, there is a place for<br></br> you within the Forge.
           </div>
 
-          <button className=" bg-[#e0ef29] min-w-[312px] py-[25px] px-[30px] text-[20px] text-center leading-[20px] -tracking-[1px] uppercase font-silkscreen text-[#030303] border-dashed border-black mt-[50px] my-[100px] border-3 hover:bg-transparent hover:text-[#000000] hover:border-[#000000]  cursor-pointer transition-all duration-300 hover:scale-105">
-              SEND US YOUR RESUME
-            </button>
+          <button className=" bg-[#e0ef29] min-w-[312px] py-[25px] px-[30px] text-[20px] text-center leading-[20px] -tracking-[1px] uppercase font-silkscreen text-[#030303] border-dashed border-black mt-[20px] lg:mt-[50px] my-[50px] lg:my-[100px] border-3 hover:bg-transparent hover:text-[#000000] hover:border-[#000000]  cursor-pointer transition-all duration-300 hover:scale-105">
+            SEND US YOUR RESUME
+          </button>
         </div>
 
             <div className="w-full relative overflow-hidden">
-                <div className="w-[90%] max-w-[1550px]  mx-auto mt-[100px] md:mt-[100px] ">
-                    <h1 className=" text-black text-[36px] lg:text-[45px] font-normal text-left font-special-gothic-expanded-one -tracking-[2.25px]">
+                <div className="w-[90%] max-w-[1550px]  mx-auto mt-[50px] lg:mt-[100px] ">
+                    <h1 className=" text-black text-[35px] lg:text-[45px] font-normal text-left font-special-gothic-expanded-one -tracking-[2.25px]">
                     AVAILABLE POSITIONS
                     </h1>
                     <div className="mt-[24px] lg:mt-[32px]">
@@ -355,40 +329,40 @@ export default function CareersPage() {
             </div>
 
 
-        <div className="w-[90%] max-w-[1550px] mx-auto mt-[100px] mb-[100px]">
+        <div className="w-[90%] max-w-[1550px] mx-auto lg:mt-[100px] lg:mb-[100px]  mt-[50px] mb-[50px]">
           {jobListings.length === 0 ? (
             <div className="flex flex-col  border border-black">
               <div className="font-silkscreen text-[15px] text-center my-[37px]">
-              There are no open positions right now <br></br><br></br>
-              We kindly ask you to revisit our website at a later date to explore any available job openings<br></br>
-              that may be active at that time. Your interest is greatly appreciated!
+                There are no open positions right now <br></br><br></br>
+                We kindly ask you to revisit our website at a later date to explore any available job openings<br></br>
+                that may be active at that time. Your interest is greatly appreciated!
               </div>
             </div>
           ) : (
-            <div className="grid grid-cols-3 gap-[50px]">
+            <div className="grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 gap-[50px]">
               {jobListings.map((category) =>
                 category.jobs.map((job) => (
                   <div key={job.title} className="flex flex-col py-[27.7px] px-[28.8px] border border-black hover:bg-black hover:text-white transition-colors duration-300 group">
-                    <span className="font-special-gothic-expanded-one text-left text-[40px] group-hover:text-white -leading-[2] uppercase">
+                    <span className="font-special-gothic-expanded-one text-left lg:text-[40px] text-[20px] group-hover:text-white -leading-[2] uppercase">
                       {job.title}
                     </span>
-                    <div className="flex flex-row gap-[11.8px] mt-[30px]">
-                      <div className="border-3 border-black px-[11.8px] py-[6px] font-inclusive-sans text-[16px] group-hover:border-white group-hover:text-white font-[600] uppercase">
+                    <div className="flex flex-row gap-[11.8px] lg:mt-[30px] mt-[20px]">
+                      <div className="border-3 border-black px-[11.8px] py-[6px] font-inclusive-sans lg:text-[16px] text-[10px] group-hover:border-white group-hover:text-white font-[600] uppercase">
                         {job.type}
                       </div>
-                      <div className="border-3 border-black px-[11.8px] py-[6px] font-inclusive-sans text-[16px] group-hover:border-white group-hover:text-white font-[600] uppercase">
+                      <div className="border-3 border-black px-[11.8px] py-[6px] font-inclusive-sans lg:text-[16px] text-[10px] group-hover:border-white group-hover:text-white font-[600] uppercase">
                         {job.location}
                       </div>
                     </div>
-                    <div className="font-inclusive-sans text-[24px] mt-[30px] group-hover:text-white uppercase mb-[100px]">
+                    <div className="font-inclusive-sans text-[18px] lg:mt-[30px] mt-[15px] group-hover:text-white uppercase lg:mb-[100px] mb-[50px]">
                       {job.description}
                     </div>
-        <div className="mt-auto">
-          <button className="bg-[#e0ef29] py-[25px] px-[30px] text-[20px] text-center -tracking-[1px] uppercase font-silkscreen text-[#030303] border-3 border-dashed border-black group-hover:bg-white group-hover:text-black group-hover:border-black flex items-center gap-2  transition-all duration-300 hover:scale-105 cursor-pointer" onClick={() => job.link && window.open(job.link, '_blank')}>
-            Apply Now
-            <ArrowUpRight size={22} />
-          </button>
-        </div>
+                    <div className="mt-auto">
+                      <button className="bg-[#e0ef29] lg:py-[25px] py-[15px] lg:px-[30px] px-[15px] lg:text-[20px] text-[15px] text-center -tracking-[1px] uppercase font-silkscreen text-[#030303] border-3 border-dashed border-black group-hover:bg-white group-hover:text-black group-hover:border-black flex items-center gap-2  transition-all duration-300 hover:scale-105 cursor-pointer" onClick={() => job.link && window.open(job.link, '_blank')}>
+                        Apply Now
+                        <ArrowUpRight size={22} />
+                      </button>
+                    </div>
                   </div>
                 ))
               )}
@@ -399,33 +373,33 @@ export default function CareersPage() {
 
         <div className="bg-black ">
 
-  <div className="w-[90%] max-w-[1550px] mx-auto py-[100px]">
-    {/* Header section */}
-    <div className="flex justify-between ">
-      <span className="text-[96px] uppercase font-special-gothic-expanded-one text-white -tracking-[4.8px]">
-        Rite of Entry
-      </span>
-      <span className="text-[36px] uppercase font-special-gothic-expanded-one text-white">
-        hiring process
-      </span>
-    </div>
+          <div className="w-[90%] max-w-[1550px] mx-auto py-[100px]">
+            {/* Header section */}
+            <div className="flex justify-between ">
+              <span className="lg:text-[96px] md:text-[66px] text-[36px] uppercase font-special-gothic-expanded-one text-white lg:-tracking-[4.8px] -tracking-[0.8px]">
+                Rite of Entry
+              </span>
+              <span className="lg:text-[36px] text-[20px] uppercase font-special-gothic-expanded-one text-white">
+                hiring process
+              </span>
+            </div>
 
-    {/* Content section */}
-    <div className="mt-[30px] text-left">
-      <div className="text-[15px] leading-[20px] uppercase font-silkscreen text-white">
-        through signal, ritual, and intent — the few become part of the whole.
-      </div>
+            {/* Content section */}
+            <div className="mt-[30px] text-left">
+              <div className="lg:text-[15px]  text-[12px] leading-[20px] uppercase font-silkscreen text-white">
+                through signal, ritual, and intent — the few become part of the whole.
+              </div>
 
-      <div className="text-[40px] mt-[56px] uppercase font-inclusive-sans text-white">
-        Joining Coullax is not a transaction — it is a<br></br>transition. We don&apos;t merely review resumes; we<br></br>decipher intent, resonance, and potential. Our<br></br>process is built to uncover builders who think<br></br> beyond the visible . individuals who feel the pulse<br></br> of systems not yet formed.
-      </div>
+              <div className="lg:text-[40px] text-[20px] mt-[36px] lg:mt-[56px] uppercase font-inclusive-sans text-white">
+                Joining Coullax is not a transaction — it is a<br></br>transition. We don&apos;t merely review resumes; we<br></br>decipher intent, resonance, and potential. Our<br></br>process is built to uncover builders who think<br></br> beyond the visible . individuals who feel the pulse<br></br> of systems not yet formed.
+              </div>
 
-      <button className="bg-[#e0ef29] min-w-[312px] py-[25px] px-[30px] text-[20px] text-center leading-[20px] -tracking-[1px] uppercase font-silkscreen text-[#030303]  border-dashed border-black mt-[50px] my-[100px] border-2 hover:bg-transparent hover:text-[#e0ef29] hover:border-[#e0ef29] hover:scale-105  transition-all duration-300 cursor-pointer">
-        SEND US YOUR RESUME
-      </button>
-    </div>
+              <button className="bg-[#e0ef29] min-w-[312px] lg:py-[25px] py-[15px] lg:px-[30px] px-[10px] lg:text-[20px] text-[15px] text-center leading-[20px] -tracking-[1px] uppercase font-silkscreen text-[#030303]  border-dashed border-black mt-[50px] my-[100px] border-2 hover:bg-transparent hover:text-[#e0ef29] hover:border-[#e0ef29] hover:scale-105  transition-all duration-300 cursor-pointer">
+                SEND US YOUR RESUME
+              </button>
+            </div>
 
-            <div className="grid grid-cols-4 gap-[25.4px]">
+            <div className="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-[25.4px]">
               {recruitmentSteps.map((step, index) => (
                 <div
                   key={index}
@@ -442,17 +416,8 @@ export default function CareersPage() {
             </div>
 
 
-  </div>
-</div>
-
-
-  
-
-
-
-
-
-
+          </div>
+        </div>
 
             <Footer/>
         </div>
