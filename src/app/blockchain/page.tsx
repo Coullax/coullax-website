35161f82957338d8ab88ce35cf669ed83d637b16
Madhawa Awishka <madhawaawishka@gmail.com>
2025-06-19 17:50:46 +0530

    Update BlockchainPage component with responsive text adjustments and button styling

--- src/app/blockchain/page.tsx
+++ src/app/blockchain/page.tsx
@@ -486,11 +486,11 @@ export default function BlockchainPage() {
 
       <div className=" py-[6.711vh] w-full bg-black">
         <div className=" w-[90%] max-w-[104.027vh] mx-auto">
-          <div className=" w-full flex flex-row justify-between items-start">
-            <h1 className=" font-special-gothic-expanded-one lg:text-[6.443vh] text-[3.443vh] leading-[7.477vh] -tracking-[0.322vh] uppercase text-white">
+          <div className="w-full flex flex-row justify-between items-start">
+            <h1 className=" font-special-gothic-expanded-one lg:text-[6.443vh] md:text-[5.443vh] text-[2.443vh] leading-[7.477vh] -tracking-[0.322vh] uppercase text-white">
               constructs
             </h1>
-            <h1 className=" font-special-gothic-expanded-one text-[2.416vh] leading-[2.805vh] -tracking-[0.121vh] uppercase text-white">
+            <h1 className=" font-special-gothic-expanded-one lg:text-[2.416vh] md:text-[1.416vh] text-[1.016vh] leading-[2.805vh] -tracking-[0.121vh] uppercase text-white">
               services
             </h1>
           </div>
@@ -506,7 +506,7 @@ export default function BlockchainPage() {
               }
             </TypingAnimation>
           </div>
-          <div className="font-inclusive-sans text-[2.685vh] leading-[3.49vh] my-[3.356vh] uppercase text-white">
+          <div className="font-inclusive-sans lg:text-[2.685vh] md:text-[1.685vh] text-[0.985vh]  leading-[1.49vh] md:leading-[2.49vh] lg:leading-[3.49vh] lg:my-[3.356vh] md:my-[3.056vh]  my-[2.356vh] uppercase text-white text-center lg:text-left">
             {constructsText.map((item, index) => (
               <ScrollAnimatedText
                 key={`constructs-${index + 1}-${activeCognitionIndex}`}
@@ -516,17 +516,19 @@ export default function BlockchainPage() {
               </ScrollAnimatedText>
             ))}
           </div>
-          <button
-            onClick={handleBookCallClick}
-            className="cursor-pointer bg-[#ff8410] py-[1.678vh] px-[5.772vh] text-black uppercase text-[1.342vh] font-silkscreen text-center leading-[1.315vh] -tracking-[0.067vh] border-2 border-dashed border-black hover:bg-transparent hover:scale-105 hover:text-[#ff8410] hover:border-[#ff8410] transition-all duration-300 ease-in-out"
-          >
-            book a call
-          </button>
-          <div className=" w-full mt-[6.711vh] grid grid-cols-3 gap-[1.342vh]">
+          <div className="flex justify-center md:justify-start">
+            <button
+              onClick={handleBookCallClick}
+              className="cursor-pointer bg-[#ff8410] py-[1.278vh] lg:py-[1.678vh] md:px-[4.772vh] px-[3.772vh] lg:px-[5.772vh] text-black uppercase text-[1.342vh] font-silkscreen text-center leading-[1.315vh] -tracking-[0.067vh] border-2 border-dashed border-black hover:bg-transparent hover:scale-105 hover:text-[#ff8410] hover:border-[#ff8410] transition-all duration-300 ease-in-out"
+            >
+              book a call
+            </button>
+          </div>
+          <div className=" w-full mt-[2.711vh] md:mt-[4.711vh] lg:mt-[6.711vh] grid grid-cols-1 md:grid-cols-2  lg:grid-cols-3 gap-[1.342vh]">
             {constructsItems.map((item, index) => (
               <div
                 key={index}
-                className=" group p-[2.685vh] min-h-[40.268vh] flex flex-col justify-start bg-transparent border border-white overflow-hidden relative"
+                className=" group p-[0.985vh] lg:p-[2.685vh] md:p-[1.685vh] min-h-[40.268vh] flex flex-col justify-start bg-transparent border border-white overflow-hidden relative"
               >
                 <div className=" h-[0px] group-hover:h-[201.342vh] transition-all duration-800 aspect-square rounded-full absolute bottom-0 translate-y-1/2 left-0 -translate-x-1/2 bg-white"></div>
                 <div className=" z-10">
