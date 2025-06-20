1e07e8aa51cdf7a2f77ae005924539a1b576779a
Madhawa Awishka <madhawaawishka@gmail.com>
2025-06-20 09:33:57 +0530

    Update BlockchainPage component with responsive layout adjustments and text size improvements

--- src/app/blockchain/page.tsx
+++ src/app/blockchain/page.tsx
@@ -486,11 +486,11 @@ export default function BlockchainPage() {
 
       <div className=" py-[6.711vh] w-full bg-black">
         <div className=" w-[90%] max-w-[104.027vh] mx-auto">
-          <div className="w-full flex flex-row justify-between items-start">
-            <h1 className=" font-special-gothic-expanded-one lg:text-[6.443vh] md:text-[5.443vh] text-[2.443vh] leading-[7.477vh] -tracking-[0.322vh] uppercase text-white">
+          <div className="w-full flex flex-col lg:flex-row justify-between lg:items-start items-center">
+            <h1 className=" font-special-gothic-expanded-one lg:text-[6.443vh] md:text-[5.443vh] text-[2.943vh] lg:leading-[7.477vh] lg:-tracking-[0.322vh] -tracking-[0.122vh] uppercase text-white">
               constructs
             </h1>
-            <h1 className=" font-special-gothic-expanded-one lg:text-[2.416vh] md:text-[1.416vh] text-[1.016vh] leading-[2.805vh] -tracking-[0.121vh] uppercase text-white">
+            <h1 className=" font-special-gothic-expanded-one lg:text-[2.416vh] md:text-[1.916vh] text-[1.516vh] leading-[2.805vh] -tracking-[0.121vh] uppercase text-white">
               services
             </h1>
           </div>
@@ -499,14 +499,14 @@ export default function BlockchainPage() {
               startOnView
               supportHTML={true}
               duration={10}
-              className="text-[0.751vh] w-full text-center lg:text-left md:text-[1.271vh] leading-[1.309vh] font-silkscreen text-white"
+              className="text-[0.951vh] w-full text-center lg:text-left md:text-[1.271vh] leading-[1.309vh] font-silkscreen text-white"
             >
               {
                 "Fed with data, trained with intent — the mind begins to see beyond the static."
               }
             </TypingAnimation>
           </div>
-          <div className="font-inclusive-sans lg:text-[2.685vh] md:text-[1.685vh] text-[0.985vh]  leading-[1.49vh] md:leading-[2.49vh] lg:leading-[3.49vh] lg:my-[3.356vh] md:my-[3.056vh]  my-[2.356vh] uppercase text-white text-center lg:text-left">
+          <div className="font-inclusive-sans lg:text-[2.685vh] md:text-[1.685vh] text-[1.085vh]  leading-[1.09vh] md:leading-[2.49vh] lg:leading-[3.49vh] lg:my-[3.356vh] md:my-[3.056vh]  my-[2.356vh] uppercase text-white text-center lg:text-left">
             {constructsText.map((item, index) => (
               <ScrollAnimatedText
                 key={`constructs-${index + 1}-${activeCognitionIndex}`}
@@ -516,7 +516,7 @@ export default function BlockchainPage() {
               </ScrollAnimatedText>
             ))}
           </div>
-          <div className="flex justify-center md:justify-start">
+          <div className="flex justify-center lg:justify-start">
             <button
               onClick={handleBookCallClick}
               className="cursor-pointer bg-[#ff8410] py-[1.278vh] lg:py-[1.678vh] md:px-[4.772vh] px-[3.772vh] lg:px-[5.772vh] text-black uppercase text-[1.342vh] font-silkscreen text-center leading-[1.315vh] -tracking-[0.067vh] border-2 border-dashed border-black hover:bg-transparent hover:scale-105 hover:text-[#ff8410] hover:border-[#ff8410] transition-all duration-300 ease-in-out"
@@ -528,7 +528,7 @@ export default function BlockchainPage() {
             {constructsItems.map((item, index) => (
               <div
                 key={index}
-                className=" group p-[0.985vh] lg:p-[2.685vh] md:p-[1.685vh] min-h-[40.268vh] flex flex-col justify-start bg-transparent border border-white overflow-hidden relative"
+                className=" group p-[0.985vh] lg:p-[2.685vh] md:p-[1.685vh] min-h-[20.268vh] md:min-h-[30.268vh] lg:min-h-[40.268vh] flex flex-col justify-start bg-transparent border border-white overflow-hidden relative"
               >
                 <div className=" h-[0px] group-hover:h-[201.342vh] transition-all duration-800 aspect-square rounded-full absolute bottom-0 translate-y-1/2 left-0 -translate-x-1/2 bg-white"></div>
                 <div className=" z-10">
@@ -547,8 +547,8 @@ export default function BlockchainPage() {
                     className=" group-hover:block hidden w-[7.047vh] h-[7.047vh]"
                   />
                 </div>
-                <div className=" mt-[7.785vh] z-10">
-                  <h1 className=" font-special-gothic-expanded-one text-[2.685vh] leading-[3.49vh] uppercase group-hover:text-black text-white">
+                <div className=" mt-[3.785vh] md:mt-[5.785vh] lg:mt-[7.785vh] z-10">
+                  <h1 className=" font-special-gothic-expanded-one text-[1.685vh] md:text-[2.085vh] lg:text-[2.685vh] leading-[3.49vh] uppercase group-hover:text-black text-white">
                     {item.title1}
                     <br />
                     {item.title2}
